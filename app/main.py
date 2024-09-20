33c9789c889cc9143aef6d562a89da442ad20eb3
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-20 09:43:51 +0530

    done

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    (clientsocket, address)  = server.accept() # wait for client
+    clientsocket.send(b'00 00 00 07')
 
 
 if __name__ == "__main__":
