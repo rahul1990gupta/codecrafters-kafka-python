d575a360f8b52c7346c5076f3b122353bde1dc36
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-20 13:36:24 +0530

    done

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ ERROR_CODE_UNSUPPORTED_VERSION = 35  # Error code for unsupported version
 def prepare_header(correlation_id):
     pass
 
+
 def prepare_body_for_apiversion(request_api_version):
     response = b""
 
@@ -34,6 +35,37 @@ def prepare_body_for_apiversion(request_api_version):
     
     return response
 
+"""
+Fetch Response (Version: 16) => throttle_time_ms error_code session_id [responses] TAG_BUFFER 
+  throttle_time_ms => INT32
+  error_code => INT16
+  session_id => INT32
+  responses => topic_id [partitions] TAG_BUFFER 
+    topic_id => UUID
+    partitions => partition_index error_code high_watermark last_stable_offset log_start_offset [aborted_transactions] preferred_read_replica records TAG_BUFFER 
+      partition_index => INT32
+      error_code => INT16
+      high_watermark => INT64
+      last_stable_offset => INT64
+      log_start_offset => INT64
+      aborted_transactions => producer_id first_offset TAG_BUFFER 
+        producer_id => INT64
+        first_offset => INT64
+      preferred_read_replica => INT32
+      records => COMPACT_RECORDS
+"""
+
+
+def prepare_body_for_fetch(request_api_version):
+    body = b""
+    body += int(0).to_bytes(4, byteorder="big") # throttle
+    body += int(0).to_bytes(2 byteorder="big") # error code 
+    body += int(0).to_bytes(4, byteorder="big") # session_id  
+    body += int(0).to_bytes(4, byteorder="big") # num responses  
+    body += int(0).to_bytes(1, byteorder="big") # tag buffer
+
+    return body
+
 
 def handle_client(clientsocket, addr):
     while True:
@@ -47,17 +79,28 @@ def handle_client(clientsocket, addr):
         print(f"message_length: {mlen} \n request_api_key: {request_api_key} \n" +
                 f"request_api_version: {request_api_version}\n" +
                 f"correlation_id: {correlation_id}")
-        if data:
-            correlation_id = correlation_id.to_bytes(4, byteorder="big")
-            print(f"correlation_id: {correlation_id}")
+        
+        header = correlation_id.to_bytes(4, byteorder="big")
+        if request_api_key ==1: # fetch
+            header += int(0).to_bytes(1, byteorder="big")
 
-            response = correlation_id + prepare_body_for_apiversion(request_api_version)
+        body = b""
+        
+        if request_api_key == 18: # api version
+            body = prepare_body_for_apiversion(request_api_version)
+        if request_api_key == 1: # fetch
+            body = prepare_body_for_fetch(request_api_version)
+        
+        print(f"header: {header}")
 
-            print(f"response: {response}") 
-            message_length = len(response).to_bytes(4, byteorder="big")
-            response = message_length + response
-            print(f"response sent: {response}")
-            clientsocket.sendall(response)
+        response = header + body # prepare_body_for_fetch(request_api_version)
+
+
+        print(f"response: {response}") 
+        message_length = len(response).to_bytes(4, byteorder="big")
+        response = message_length + response
+        print(f"response sent: {response}")
+        clientsocket.sendall(response)
 
 
 def main():
