e6219f33500b8785b05722900b9239266e9d0b2f
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-20 12:09:31 +0530

    done

--- app/main.py
+++ app/main.py
@@ -29,10 +29,12 @@ def handle_client(clientsocket):
                 response += int(0).to_bytes(2, byteorder="big")
             
             print(f"response: {response}") 
-            response += int(2).to_bytes(1, byteorder="big")
+            response += int(2).to_bytes(1, byteorder="big") # num api keys
             response += request_api_key.to_bytes(2, byteorder="big")
             response += request_api_version.to_bytes(2, byteorder="big")
             response += request_api_version.to_bytes(2, byteorder="big")
+            response += int(0).to_bytes(2, byteorder="big") # tag buffer
+            
             response += int(0).to_bytes(4, byteorder="big")
 
             print(f"response: {response}") 
