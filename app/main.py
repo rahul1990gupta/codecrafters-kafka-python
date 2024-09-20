a693ab316605f00ad4dbbbdc4942b9bc10663261
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-20 10:11:16 +0530

    done

--- app/main.py
+++ app/main.py
@@ -2,17 +2,8 @@ import socket  # noqa: F401
 import struct
 
 
-def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
-    server = socket.create_server(("localhost", 9092), reuse_port=True)
-    (clientsocket, address)  = server.accept() # wait for client
-    if clientsocket:
-        print(f"connect by {address}")
+def handle_client(clientsocket):
+    with clientsocket:
         data = clientsocket.recv(1024)
         if data:
             correlation_id = int(7).to_bytes(4, byteorder="big")
@@ -21,6 +12,23 @@ def main():
             response = message_length + correlation_id
             clientsocket.sendall(response)
             print(f"response sent: {response}")
+
+
+
+def main():
+    # You can use print statements as follows for debugging,
+    # they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    # Uncomment this to pass the first stage
+    #
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+
+    while True:
+        (clientsocket, address)  = server.accept() # wait for client
+        if clientsocket:
+            print(f"connect by {address}")
+            handle_client(clientsocket)
 
 
 if __name__ == "__main__":
