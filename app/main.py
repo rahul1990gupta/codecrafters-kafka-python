f21f6f8a7546dbc992ceabfa2587a1edb3c6ce01
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-20 11:34:44 +0530

    done

--- app/main.py
+++ app/main.py
@@ -29,9 +29,9 @@ def handle_client(clientsocket):
                 response += int(0).to_bytes(2, byteorder="big")
             
             print(f"response: {response}") 
-            response += request_api_key.to_bytes(2, byeorder="big")
-            response += request_api_version.to_bytes(2, byeorder="big")
-            response += request_api_version.to_bytes(2, byeorder="big")
+            response += request_api_key.to_bytes(2, byteorder="big")
+            response += request_api_version.to_bytes(2, byteorder="big")
+            response += request_api_version.to_bytes(2, byteorder="big")
 
             print(f"response: {response}") 
             message_length = len(response).to_bytes(4, byteorder="big")
